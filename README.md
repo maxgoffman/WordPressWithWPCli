a54f8104d2161bfebbdabd591325ab2a813b2316
Maxi <maxgoffman@gmail.com>
2020-09-30 12:11:07 -0300

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# WordPressWithWPCli
+# WordPress With WP CLI Docker Project
+A simple docker compose example project that implements a WordPress Image with WP Cli installed.
+
+## Installation
+Prerequisites: Docker and Docker-compose up<br />
+If you want to use it to migrate a site, you can do so by dumping your site database into ./mysql-dump/sitedumped.sql and uncommenting line 4 of ./docker/install (and line 6 if you need to set a different url).<br />
+If you want to install a number of plugins you can do so by adding them in ./docker/install there is an example of such command in line 10.<br />
+Steps:<br />
+1- In your terminal type docker-compose up -d<br />
+2- Next, type docker/install
+
+## Author
+[Max Goffman] (https://github.com/maxgoffman)
---
+++ wp-config.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'wordpress');
+
+/** MySQL database username */
+define( 'DB_USER', 'wordpress');
+
+/** MySQL database password */
+define( 'DB_PASSWORD', 'wordpress');
+
+/** MySQL hostname */
+define( 'DB_HOST', 'db');
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         'bb3443c8fcccf85b153e5cdf1f611cd158e7cd0e');
+define( 'SECURE_AUTH_KEY',  'f86dc2141014082ca6a829d16d5d6a63d04bbc2b');
+define( 'LOGGED_IN_KEY',    '75039567d1c5e0215d5d865ab7ef2a05ee4738c0');
+define( 'NONCE_KEY',        '788a70344819d92ea5ef0f50bd3c332a0644069d');
+define( 'AUTH_SALT',        'c3e794dee6d20d0a02d120103657f23ffe703e2f');
+define( 'SECURE_AUTH_SALT', 'ef2e7e82f8e2dd5d20437860a6e50f83fcccdc0b');
+define( 'LOGGED_IN_SALT',   '56b4dca07bc838526c60869d6d25292bd4d0da57');
+define( 'NONCE_SALT',       'b52a24d57e1c1046a58b9757ac0aaf7f1b4ad2fa');
+define('FS_METHOD', 'direct');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define( 'WP_DEBUG', false );
+
+// If we're behind a proxy server and using HTTPS, we need to alert WordPress of that fact
+// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+	$_SERVER['HTTPS'] = 'on';
+}
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once( ABSPATH . 'wp-settings.php' );
